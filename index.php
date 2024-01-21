40f3dd4944e55e284f3746397f4c3da4c6ca2664
Felipe <lipestaub@gmail.com>
2024-01-21 20:21:21 -0300

    WIP

--- index.php
+++ index.php
@@ -4,6 +4,22 @@
     require_once __DIR__  . '/modules/calcularResultadoMultiplicacaoEntreMatrizes.php';
     require_once __DIR__ . '/modules/mostrarMatriz.php';
 
-    mostrarMatriz(calcularResultadoMultiplicacaoEntreMatrizes(MATRIZ_QUADRADA_A, MATRIZ_QUADRADA_B));
-    mostrarMatriz(calcularResultadoMultiplicacaoEntreMatrizes(MATRIZ_NAO_QUADRADA_A, MATRIZ_NAO_QUADRADA_B));
+    $a = MATRIZ_QUADRADA_A;
+    $b = MATRIZ_QUADRADA_B;
+
+    /*for ($linha = 0; $linha < count($a); $linha++) {
+        for ($coluna = 0; $coluna <= count($a); $coluna++) {
+
+        }
+    }*/
+
+    $matrizResultados[0][0] = ($a[0][0] * $b[0][0]) + ($a[0][1] * $b[1][0]);
+    $matrizResultados[0][1] = ($a[0][0] * $b[0][1]) + ($a[0][1] * $b[1][1]);
+    $matrizResultados[1][0] = ($a[1][0] * $b[0][0]) + ($a[1][1] * $b[1][0]);
+    $matrizResultados[1][1] = ($a[1][0] * $b[0][1]) + ($a[1][1] * $b[1][1]);
+
+    mostrarMatriz($matrizResultados);
+
+    //mostrarMatriz(calcularResultadoMultiplicacaoEntreMatrizes(MATRIZ_QUADRADA_A, MATRIZ_QUADRADA_B));
+    //mostrarMatriz(calcularResultadoMultiplicacaoEntreMatrizes(MATRIZ_NAO_QUADRADA_A, MATRIZ_NAO_QUADRADA_B));
 ?>
--- modules/calcularResultadoMultiplicacaoEntreMatrizes.php
+++ modules/calcularResultadoMultiplicacaoEntreMatrizes.php
@@ -1,13 +1,28 @@
 <?php
     function calcularResultadoMultiplicacaoEntreMatrizes(array $matrizA, array $matrizB) : array {
+        $linhaResultado = 0;
+        $colunaResultado = 0;
         $resultados = [];
+        $count = 1;
 
         for ($i = 0; $i < count($matrizA); $i++) {
+            $resultado = 0;
+
+            echo "\n\nVOLTA " . $count . "\n\n";
             for ($j = 0; $j < count($matrizA[$i]); $j++) {
-                $resultado = $matrizA[$i]
+                echo "matrizA[$i][$j] (" . $matrizA[$i][$j] . ") * matrizB[$j][$i] (" . $matrizB[$j][$i] . ") = ";
+                $resultado += $matrizA[$i][$j] * $matrizB[$j][$i];
+                echo $resultado . "\n\n";
             }
 
-            $resultados[$i][$j] = $resultado;
+            for ($j = 0; $j < count($matrizA[$i]); $j++) {
+                echo "matrizA[$i][$j] (" . $matrizA[$i][$j] . ") * matrizB[$j][$i] (" . $matrizB[$j][$i] . ") = ";
+                $resultado += $matrizA[$i][$j] * $matrizB[$j][$i];
+                echo $resultado . "\n\n";
+            }
+            $count++;
+            echo $resultado . "\n";
+            //$resultados[$i][$j] = $resultado;
         }
     
         return $resultados;
