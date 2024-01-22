b4d9b6d3ab8b179dc923b6f17cc48499ce4e9cb3
Felipe Staub~ <lipestaub@gmail.com>
2024-01-22 08:58:01 -0300

    done

--- modules/calcularResultadoMultiplicacaoEntreMatrizes.php
+++ modules/calcularResultadoMultiplicacaoEntreMatrizes.php
@@ -1,28 +1,13 @@
 <?php
     function calcularResultadoMultiplicacaoEntreMatrizes(array $matrizA, array $matrizB) : array {
-        $linhaResultado = 0;
-        $colunaResultado = 0;
-        $resultados = [];
-        $count = 1;
-
         for ($i = 0; $i < count($matrizA); $i++) {
-            $resultado = 0;
-
-            echo "\n\nVOLTA " . $count . "\n\n";
-            for ($j = 0; $j < count($matrizA[$i]); $j++) {
-                echo "matrizA[$i][$j] (" . $matrizA[$i][$j] . ") * matrizB[$j][$i] (" . $matrizB[$j][$i] . ") = ";
-                $resultado += $matrizA[$i][$j] * $matrizB[$j][$i];
-                echo $resultado . "\n\n";
-            }
-
-            for ($j = 0; $j < count($matrizA[$i]); $j++) {
-                echo "matrizA[$i][$j] (" . $matrizA[$i][$j] . ") * matrizB[$j][$i] (" . $matrizB[$j][$i] . ") = ";
-                $resultado += $matrizA[$i][$j] * $matrizB[$j][$i];
-                echo $resultado . "\n\n";
-            }
-            $count++;
-            echo $resultado . "\n";
-            //$resultados[$i][$j] = $resultado;
+            for ($j = 0; $j < count($matrizB[0]); $j++) {
+                $resultado = 0;
+                for ($k = 0; $k < count($matrizA[0]); $k++) {
+                    $resultado += $matrizA[$i][$k] * $matrizB[$k][$j];
+                }          
+                $resultados[$i][$j] = $resultado;
+            }            
         }
     
         return $resultados;
