f8412ffc81c61a3513a9327dd6d0b6e91c933b29
Felipe <lipestaub@gmail.com>
2024-01-23 22:01:39 -0300

    update

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
     require_once __DIR__ . '/modules/constants.php';
-    require_once __DIR__ . '/modules/variables.php';
     require_once __DIR__  . '/modules/calcularResultadoMultiplicacaoEntreMatrizes.php';
     require_once __DIR__ . '/modules/mostrarMatriz.php';
 
+    echo "Matriz quadrada:";
     mostrarMatriz(calcularResultadoMultiplicacaoEntreMatrizes(MATRIZ_QUADRADA_A, MATRIZ_QUADRADA_B));
+    echo "Matriz não quadrada:";
     mostrarMatriz(calcularResultadoMultiplicacaoEntreMatrizes(MATRIZ_NAO_QUADRADA_A, MATRIZ_NAO_QUADRADA_B));
 ?>
